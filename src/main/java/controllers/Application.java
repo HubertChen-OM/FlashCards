a84a02facdd5529f2d5a987e5f79a4a7e71a6824
HubertChen-OM <HubertChen-OM@users.noreply.github.com>
2018-12-05 17:07:21 -0500

    Update Application.java

--- src/main/java/controllers/Application.java
+++ src/main/java/controllers/Application.java
@@ -33,5 +33,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.exit(1);
+		System.out.println("ASDASD");
+		asdflajsdfljsad
 	}
 }
